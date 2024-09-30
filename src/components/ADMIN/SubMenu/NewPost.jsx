2dc4827c4057292d831bf5fa75896085728c3c4c
HuynhQuiLe <lehuynhqui1996@gmail.com>
2024-09-30 10:44:55 +0700

    chinh sua submenu cho news-post

--- src/components/ADMIN/SubMenu/NewPost.jsx
+++ src/components/ADMIN/SubMenu/NewPost.jsx
@@ -19,21 +19,59 @@ const NewPost = () => {
           <div className=" cursor-pointer">
             <div>
               <img
-                src="/img/X-white.svg"
+                src="/img/KAB.svg"
                 alt="small logo"
-                // className={`${15}`}
-                style={{ width: `15px` }}
+                className="h-[10px] w-auto"
               />
             </div>
           </div>
-          <p className="ml-1 mr-3">new-post</p>
+          <p className="mr-3">.new-post</p>
           <i className=" cursor-pointer fa fa-bars"></i>
         </div>
       </div>
       {!isShow && (
         <div className="ml-[6px] bg-[#3e3e3e] text-white rounded-[8px] h-[60px] flex items-center px-[6px]">
-          {/* {pathname.replaceAll("/", "") === "" && <Home />} */}
-          Home
+          <div className="h-[48px] flex">
+            <div
+              className="h-full flex items-center px-[12px] ml-[6px] rounded-[8px] border transition-default cursor-pointer 
+              border-[#4e4e4e] hover:border-[#a7a7a7]"
+            >
+              General
+            </div>
+            <div
+              className="h-full flex items-center px-[12px] ml-[6px] rounded-[8px] border transition-default cursor-pointer 
+              border-[#4e4e4e] hover:border-[#a7a7a7]"
+            >
+              Title
+            </div>
+            <div
+              className="h-full flex items-center px-[12px] ml-[6px] rounded-[8px] border transition-default cursor-pointer 
+              border-[#4e4e4e] hover:border-[#a7a7a7]"
+            >
+              Cover
+            </div>
+            <div
+              className="h-full flex items-center px-[12px] ml-[6px] rounded-[8px] border transition-default cursor-pointer 
+              border-[#4e4e4e] hover:border-[#a7a7a7]"
+            >
+              Content
+            </div>
+          </div>
+        </div>
+      )}
+
+      {!isShow && (
+        <div className="ml-[6px] bg-error text-black rounded-[8px] h-[60px] flex items-center px-[6px] cursor-pointer">
+          <p className="flex items-center px-[20px] font-semibold text-nowrap text-white ">
+            Draft
+          </p>
+        </div>
+      )}
+      {!isShow && (
+        <div className="ml-[6px] bg-lemon text-black rounded-[8px] h-[60px] flex items-center px-[6px] cursor-pointer">
+          <p className="flex items-center px-[20px] font-semibold ">
+            Preview <i className="fa fa-external-link-alt ml-1"></i>
+          </p>
         </div>
       )}
     </section>
